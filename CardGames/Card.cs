1f473a817ee25423896b64229e681585c16c6633
Austin Stutzman <astutz96.development@protonmail.com>
2021-03-19 19:54:20 -0400

    moving logic out of Card and into Program

--- CardGames/Card.cs
+++ CardGames/Card.cs
@@ -12,52 +12,35 @@ namespace CardGames
     {
 
         private Suit suit;
-        private int rank;
+        private int rankValue;
 
-        public Card(Suit suit, int rank)
+        public int RankValue
         {
-            if (rank < 2 || rank > 14)
+            get { return rankValue; }
+            set { rankValue = value; }
+        }
+
+        public Suit Suit
+        {
+            get { return suit; }
+            set { suit = value; }
+        }
+
+        public Card(Suit suit, int rankValue)
+        {
+            if (rankValue < 2 || rankValue > 14)
             {
                 throw new ArgumentException("Invalid Rank Provided: " + rank.ToString());
             };
 
             this.suit = suit;
-            this.rank = rank;
+            this.rankValue = rankValue;
         }
 
-        public int GetRankValue()
-        {
-            return rank;
-        }
+        //     public string GetSuit()
+        //     {
+        //         return Enum.GetName(typeof(Suit), this.suit);
+        //     }
 
-        public string GetRank()
-        {
-            if (rank.Equals(11))
-            {
-                return "Jack";
-            }
-            else if (rank.Equals(12))
-            {
-                return "Queen";
-            }
-            else if (rank.Equals(13))
-            {
-                return "King";
-            }
-            else if (rank.Equals(14))
-            {
-                return "Ace";
-            }
-            else
-            {
-                return rank.ToString();
-            }
-        }
-
-        public string GetSuit()
-        {
-            return Enum.GetName(typeof(Suit), this.suit);
-        }
-
+        // }
     }
-}
--- Program.cs
+++ Program.cs
@@ -7,55 +7,81 @@ namespace GameOfWar
     {
         static void Main(string[] args)
         {
-            Deck player1Deck = new Deck();
-            Deck player2Deck = new Deck();
-            int p1RoundWinCount = 0;
-            int p2RoundWinCount = 0;
 
-            player1Deck.Shuffle();
-            player2Deck.Shuffle();
-            player2Deck.Shuffle();
-            player2Deck.Shuffle();
+            // Deck player1Deck = new Deck();
+            // Deck player2Deck = new Deck();
+            // int p1RoundWinCount = 0;
+            // int p2RoundWinCount = 0;
 
-            while (!(player1Deck.GetCards().Count.Equals(5) || player2Deck.GetCards().Count.Equals(5)))
+            // player1Deck.Shuffle();
+            // player2Deck.Shuffle();
+            // player2Deck.Shuffle();
+            // player2Deck.Shuffle();
+
+            // while (!(player1Deck.GetCards().Count.Equals(5) || player2Deck.GetCards().Count.Equals(5)))
+            // {
+            //     Card p1Card = player1Deck.DrawTopCard();
+            //     Card p2Card = player2Deck.DrawTopCard();
+            //     Console.Write(p1Card.GetRank() + " of " + p1Card.GetSuit());
+            //     Console.Write(" VS ");
+            //     Console.Write(p2Card.GetRank() + " of " + p2Card.GetSuit());
+            //     if (p1Card.GetRankValue() > p2Card.GetRankValue())
+            //     {
+            //         p1RoundWinCount++;
+            //         Console.Write(" Player 1 wins round!");
+            //         player1Deck.AddCardToBottomOfDeck(p1Card);
+            //         player1Deck.AddCardToBottomOfDeck(p2Card);
+            //     }
+            //     else if (p1Card.GetRankValue() < p2Card.GetRankValue())
+            //     {
+            //         p2RoundWinCount++;
+            //         Console.Write(" Player 2 wins round!");
+            //         player2Deck.AddCardToBottomOfDeck(p1Card);
+            //         player2Deck.AddCardToBottomOfDeck(p2Card);
+            //     }
+            //     else
+            //     {
+            //         p1RoundWinCount++;
+            //         Console.Write("--- TIE ---");
+            //         player1Deck.AddCardToBottomOfDeck(p1Card);
+            //         player1Deck.AddCardToBottomOfDeck(p2Card);
+            //     }
+            //     Console.WriteLine("---");
+            // }
+
+            // if (player1Deck.GetCards().Count.Equals(0))
+            // {
+            //     Console.WriteLine("layer 2 wins! Total rounds won: " + p1RoundWinCount.ToString());
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Player 1 wins! Total round won: " + p2RoundWinCount.ToString());
+            // }
+        }
+
+        public string GetRank(Card card)
+        {
+            if (card.RankValue.Equals(11))
             {
-                Card p1Card = player1Deck.DrawTopCard();
-                Card p2Card = player2Deck.DrawTopCard();
-                Console.Write(p1Card.GetRank() + " of " + p1Card.GetSuit());
-                Console.Write(" VS ");
-                Console.Write(p2Card.GetRank() + " of " + p2Card.GetSuit());
-                if (p1Card.GetRankValue() > p2Card.GetRankValue())
-                {
-                    p1RoundWinCount++;
-                    Console.Write(" Player 1 wins round!");
-                    player1Deck.AddCardToBottomOfDeck(p1Card);
-                    player1Deck.AddCardToBottomOfDeck(p2Card);
-                }
-                else if (p1Card.GetRankValue() < p2Card.GetRankValue())
-                {
-                    p2RoundWinCount++;
-                    Console.Write(" Player 2 wins round!");
-                    player2Deck.AddCardToBottomOfDeck(p1Card);
-                    player2Deck.AddCardToBottomOfDeck(p2Card);
-                }
-                else
-                {
-                    p1RoundWinCount++;
-                    Console.Write("--- TIE ---");
-                    player1Deck.AddCardToBottomOfDeck(p1Card);
-                    player1Deck.AddCardToBottomOfDeck(p2Card);
-                }
-                Console.WriteLine("---");
+                return "Jack";
             }
-
-            if (player1Deck.GetCards().Count.Equals(0))
+            else if (card.RankValue.Equals(12))
             {
-                Console.WriteLine("layer 2 wins! Total rounds won: " + p1RoundWinCount.ToString());
+                return "Queen";
+            }
+            else if (card.RankValue.Equals(13))
+            {
+                return "King";
+            }
+            else if (card.RankValue.Equals(14))
+            {
+                return "Ace";
             }
             else
             {
-                Console.WriteLine("Player 1 wins! Total round won: " + p2RoundWinCount.ToString());
+                return card.RankValue.ToString();
             }
         }
+
     }
 }
