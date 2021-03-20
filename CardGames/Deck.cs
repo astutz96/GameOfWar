b966b1f37083c621619ecdeaad267e7dff5160be
Austin Stutzman <astutz96.development@protonmail.com>
2021-03-19 20:43:38 -0400

    removing logic from Deck class

--- CardGames/Deck.cs
+++ CardGames/Deck.cs
@@ -5,58 +5,6 @@ namespace CardGames
 {
     public class Deck
     {
-        private List<Card> cards;
-        public Deck()
-        {
-            cards = GenerateCards();
-        }
-
-        public List<Card> GetCards()
-        {
-            return cards;
-        }
-
-        public Card DrawTopCard()
-        {
-            Card topCard;
-            topCard = cards[0];
-            cards.RemoveAt(0);
-            return topCard;
-        }
-
-        public void AddCardToBottomOfDeck(Card card)
-        {
-            cards.Add(card);
-        }
-
-        public void Shuffle()
-        {
-            //FisherYatesShuffle
-            Random rng = new Random();
-            int n = cards.Count;
-            while (n > 1)
-            {
-                n--;
-                int x = rng.Next(n + 1);
-                Card value = cards[x];
-                cards[x] = cards[n];
-                cards[n] = value;
-            }
-        }
-
-        private List<Card> GenerateCards()
-        {
-            List<Card> myCards = new List<Card>();
-
-            for (int i = 2; i < 15; i++)
-            {
-                myCards.Add(new Card(Suit.Clubs, i));
-                myCards.Add(new Card(Suit.Diamonds, i));
-                myCards.Add(new Card(Suit.Hearts, i));
-                myCards.Add(new Card(Suit.Spades, i));
-            }
-            return myCards;
-        }
 
     }
 
